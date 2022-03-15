ce6d4002df91da92d6d7c65d2995fa01e6e155a9
xerox0000 <toore@Debian.home>
2022-03-15 02:10:02 +0100

    creati modelli e generate migrations per tabelle DB :hammer:

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Prodotti(models.Model):
+    #definisco attributi che poi diventeranno colonne della mia tabella
+    #grazie al concetto delle migration posso fare delle migrazioni e applicarle al mio db
+    owner = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.TextField()
